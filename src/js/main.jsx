90b9b635a9ab74be48bece06ff742b48d5dc501b
Andri Arrieta <andriarrieta7@gmail.com>
2025-11-28 20:35:29 +0000

    Navbar creada

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,10 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <NavBar/>
   </React.StrictMode>,
 )
